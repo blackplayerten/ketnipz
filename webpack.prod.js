210dded5d2598fcf773dc12d7e9888070bd418a1
Kurganova Alexandra <kurganova06.1998@gmail.com>
2019-01-14 20:51:04 +0300

    деплой

---
+++ webpack.prod.js
@@ -0,0 +1,50 @@
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+const src = path.resolve(__dirname, 'src');
+const build = path.resolve(__dirname, 'build');
+
+const port = 8080;
+
+module.exports = {
+    entry: [
+        path.resolve(src, 'index.js'),
+    ],
+    output: {
+        path: build,
+        filename: 'bundle.js',
+        publicPath: '/',
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /(node_modules|bower_components)/,
+                use: {
+                    loader: 'babel-loader',
+                },
+            },
+            {
+                test: /\.(png|jpg|gif|jpeg)$/,
+                use: {
+                    loader: 'file-loader',
+                    options: {
+                        name: 'static/img/[name].[ext]'
+                    },
+                },
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    'style-loader',
+                    'css-loader',
+                ],
+            },
+        ],
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: path.resolve(src, 'index.html'),
+        }),
+    ],
+};
